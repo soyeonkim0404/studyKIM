c311aed867295e6524dd5a3dd81a670779f88e0f
kimsoyeon <soyeon.kim9104@gmail.com>
2022-09-14 19:03:31 +0900

    section two 작업

--- appleClone2022/dist/index.de5c0784.js
+++ appleClone2022/dist/index.de5c0784.js
@@ -1,5 +1,12 @@
 "use strict";
-(function() {
+/*product nav sticky*/ (function() {
+    const productNav = document.querySelector("#product-nav");
+    window.addEventListener("scroll", function() {
+        if (window.scrollY >= 44) productNav.classList.add("sticky-now");
+        else productNav.classList.remove("sticky-now");
+    });
+})();
+/*device color show and hide*/ (function() {
     let colorTabImage = document.querySelectorAll(".gallery-item");
     let colorTabNav = document.querySelectorAll(".nav-item");
     for(let i = 0; i < colorTabNav.length; i++)colorTabNav[i].addEventListener("click", (evt)=>{
